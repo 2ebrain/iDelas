e54289db14e2f88b179fd1554af002e84480f6c4
tripplesixblunt <leonardo02lima@gmail.com>
2021-04-19 15:57:38 -0300

    Arquivo leia adicionado com view de Detalhe

---
+++ README.md
@@ -0,0 +1,60 @@
+<h1 align="center">Welcome to iDelas 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-0.0.1-blue.svg?cacheSeconds=2592000" />
+  <a href="https://github.com/2ebrain/iDelas" target="_blank">
+    <img alt="Documentation" src="https://img.shields.io/badge/documentation-yes-brightgreen.svg" />
+  </a>
+  <a href="2ebrain" target="_blank">
+    <img alt="License: 2ebrain" src="https://img.shields.io/badge/License-2ebrain-yellow.svg" />
+  </a>
+  <a href="https://twitter.com/2ebrain" target="_blank">
+    <img alt="Twitter: 2ebrain" src="https://img.shields.io/twitter/follow/2ebrain.svg?style=social" />
+  </a>
+</p>
+
+> iDelas é um aplicativo que tem foco ajudar mulheres com investimento e finanças da sua empresa
+
+### 🏠 [Homepage](https://github.com/2ebrain/iDelas)
+
+### ✨ [Demo](https://expo.io/accounts/leo-bot/projects/idelas)
+
+## Install
+
+```sh
+yarn install
+```
+
+## Usage
+
+```sh
+yarn start
+```
+
+## Author
+
+👤 **2eBrain & Leonardo Lima**
+
+* Website: 2ebrain
+* Twitter: [@2ebrain](https://twitter.com/2ebrain)
+* Github: [@2ebrain](https://github.com/2ebrain)
+* LinkedIn: [@2ebrain](https://linkedin.com/in/2ebrain)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!<br />Feel free to check [issues page](2ebrain). You can also take a look at the [contributing guide](https://github.com/bot-leo).
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+<a href="https://www.patreon.com/2ebrain">
+  <img src="https://c5.patreon.com/external/logo/become_a_patron_button@2x.png" width="160">
+</a>
+
+## 📝 License
+
+Copyright © 2021 [2eBrain & Leonardo Lima](https://github.com/2ebrain).<br />
+This project is [2ebrain](2ebrain) licensed.
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
--- src/pages/Detalhes.js
+++ src/pages/Detalhes.js
@@ -5,17 +5,140 @@ import { StyleSheet,
           SafeAreaView, 
           ScrollView, 
           View} from 'react-native'
+import {FontAwesome5} from '@expo/vector-icons'
 
 export default function Detalhes(){
   return(
-    <SafeAreaView>
-      <ScrollView>
-        <View>
-          <Text>Detalhes do Resultado Financeiro</Text>
-          
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollContainer}>
+
+        <View style={styles.content}>
+          <View style={styles.closeBox}>
+            <Text>Fechar Detalhes</Text>
+            <FontAwesome5 name='times-circle'/>
+          </View>
+
+          <View  style={styles.contentBox}>
+            <Text>Detalhes do Resultado Financeiro</Text>
+
+            <View style={styles.boxWhite}>
+              <Text style={styles.boxText}>Quanto cada saída corresponde a 
+                    meta financeira mensal </Text>
+            </View>
+            {/* Seção de inputs */}
+            <View style={styles.boxInput}>
+              <Text>Entradas Operacionais</Text>
+              <Text>-</Text>
+              <Text>0,0%</Text>
+              <FontAwesome5 name='chevron-down'/>
+            </View>
+            <View style={styles.boxInput}>
+              <Text>Entradas Operacionais</Text>
+              <Text>-</Text>
+              <Text>0,0%</Text>
+              <FontAwesome5 name='chevron-down'/>
+            </View>
+            <View style={styles.boxInput}>
+              <Text>Entradas Operacionais</Text>
+              <Text>-</Text>
+              <Text>0,0%</Text>
+              <FontAwesome5 name='chevron-down'/>
+            </View>
+            <View style={styles.boxInput}>
+              <Text>Entradas Operacionais</Text>
+              <Text>-</Text>
+              <Text>0,0%</Text>
+              <FontAwesome5 name='chevron-down'/>
+            </View>
+            <View style={styles.boxInput}>
+              <Text>Entradas Operacionais</Text>
+              <Text>-</Text>
+              <Text>0,0%</Text>
+              <FontAwesome5 name='chevron-down'/>
+            </View>
+            <View style={styles.boxInput}>
+              <Text>Entradas Operacionais</Text>
+              <Text>-</Text>
+              <Text>0,0%</Text>
+              <FontAwesome5 name='chevron-down'/>
+            </View>
+            <View style={styles.boxInput}>
+              <Text>Entradas Operacionais</Text>
+              <Text>-</Text>
+              <Text>0,0%</Text>
+              <FontAwesome5 name='chevron-down'/>
+            </View>
+            <View style={styles.boxInput}>
+              <Text>Entradas Operacionais</Text>
+              <Text>-</Text>
+              <Text>0,0%</Text>
+              <FontAwesome5 name='chevron-down'/>
+            </View>
+            <View style={styles.boxInput}>
+              <Text>Entradas Operacionais</Text>
+              <Text>-</Text>
+              <Text>0,0%</Text>
+              <FontAwesome5 name='chevron-down'/>
+            </View>
+            <View style={styles.boxInput}>
+              <Text>Entradas Operacionais</Text>
+              <Text>-</Text>
+              <Text>0,0%</Text>
+              <FontAwesome5 name='chevron-down'/>
+            </View>
+
+          </View>
+
 
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    width:'100%',
+    height:'auto',
+    backgroundColor:'pink',
+  },
+  
+  content:{
+    flex:1,
+    width:'100%',
+    backgroundColor:'#691CC7',
+    
+  },
+  closeBox:{
+    flex:1,
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  contentBox:{
+    width:321,
+    height:'auto',
+  },
+  boxWhite:{
+    backgroundColor:'#fff',
+    borderRadius:10,
+    width:'100%',
+    height:57,
+    alignItems:'center',
+    justifyContent:'center',
+
+  },
+  boxText:{
+    textAlign:'center',
+  },
+  boxInput:{
+    flexDirection:'row',
+    width:'100%',
+    height:47,
+    justifyContent:'space-evenly',
+    alignItems:'center',
+    backgroundColor:'#fff',
+    borderRadius:10,
+  },
+});
