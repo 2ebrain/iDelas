0c095d59eba0a39b5fa766458f370bfe5bf074da
tripplesixblunt <leonardo02lima@gmail.com>
2021-04-19 17:49:37 -0300

    View de detalhes adicionada com elementos

--- src/pages/Detalhes.js
+++ src/pages/Detalhes.js
@@ -13,13 +13,18 @@ export default function Detalhes(){
       <ScrollView style={styles.scrollContainer}>
 
         <View style={styles.content}>
-          <View style={styles.closeBox}>
-            <Text>Fechar Detalhes</Text>
-            <FontAwesome5 name='times-circle'/>
+
+          <View style={styles.closeContainer}>
+            <View style={styles.closeBox}>
+              <Text style={styles.closeText}>Fechar Detalhes</Text>
+              <TouchableOpacity>
+                <FontAwesome5 name='times-circle' color='#fff' size={23}/>
+              </TouchableOpacity>
+            </View>
           </View>
 
           <View  style={styles.contentBox}>
-            <Text>Detalhes do Resultado Financeiro</Text>
+            <Text style={styles.textDetails}>Detalhes do Resultado Financeiro</Text>
 
             <View style={styles.boxWhite}>
               <Text style={styles.boxText}>Quanto cada saída corresponde a 
@@ -27,65 +32,141 @@ export default function Detalhes(){
             </View>
             {/* Seção de inputs */}
             <View style={styles.boxInput}>
-              <Text>Entradas Operacionais</Text>
-              <Text>-</Text>
-              <Text>0,0%</Text>
-              <FontAwesome5 name='chevron-down'/>
+              <View>
+                <Text style={styles.textInitial}>Entradas Operacionais</Text>
+              </View>
+              <View style={styles.boxMountante}>
+                <Text style={styles.textValor}>-</Text>
+              </View>
+              <View style={styles.boxPorcertn}>
+                <Text style={styles.textPorcen}>0,0%</Text>
+                <TouchableOpacity>
+                  <FontAwesome5 name='chevron-down'/>
+                </TouchableOpacity>
+              </View>
             </View>
             <View style={styles.boxInput}>
-              <Text>Entradas Operacionais</Text>
-              <Text>-</Text>
-              <Text>0,0%</Text>
-              <FontAwesome5 name='chevron-down'/>
+              <View>
+                <Text style={styles.textInitial}>Entradas Operacionais</Text>
+              </View>
+              <View style={styles.boxMountante}>
+                <Text style={styles.textValor}>-</Text>
+              </View>
+              <View style={styles.boxPorcertn}>
+                <Text style={styles.textPorcen}>0,0%</Text>
+                <TouchableOpacity>
+                  <FontAwesome5 name='chevron-down'/>
+                </TouchableOpacity>
+              </View>
             </View>
             <View style={styles.boxInput}>
-              <Text>Entradas Operacionais</Text>
-              <Text>-</Text>
-              <Text>0,0%</Text>
-              <FontAwesome5 name='chevron-down'/>
+              <View>
+                <Text style={styles.textInitial}>Entradas Operacionais</Text>
+              </View>
+              <View style={styles.boxMountante}>
+                <Text style={styles.textValor}>-</Text>
+              </View>
+              <View style={styles.boxPorcertn}>
+                <Text style={styles.textPorcen}>0,0%</Text>
+                <TouchableOpacity>
+                  <FontAwesome5 name='chevron-down'/>
+                </TouchableOpacity>
+              </View>
             </View>
             <View style={styles.boxInput}>
-              <Text>Entradas Operacionais</Text>
-              <Text>-</Text>
-              <Text>0,0%</Text>
-              <FontAwesome5 name='chevron-down'/>
+              <View>
+                <Text style={styles.textInitial}>Entradas Operacionais</Text>
+              </View>
+              <View style={styles.boxMountante}>
+                <Text style={styles.textValor}>-</Text>
+              </View>
+              <View style={styles.boxPorcertn}>
+                <Text style={styles.textPorcen}>0,0%</Text>
+                <TouchableOpacity>
+                  <FontAwesome5 name='chevron-down'/>
+                </TouchableOpacity>
+              </View>
             </View>
             <View style={styles.boxInput}>
-              <Text>Entradas Operacionais</Text>
-              <Text>-</Text>
-              <Text>0,0%</Text>
-              <FontAwesome5 name='chevron-down'/>
+              <View>
+                <Text style={styles.textInitial}>Entradas Operacionais</Text>
+              </View>
+              <View style={styles.boxMountante}>
+                <Text style={styles.textValor}>-</Text>
+              </View>
+              <View style={styles.boxPorcertn}>
+                <Text style={styles.textPorcen}>0,0%</Text>
+                <TouchableOpacity>
+                  <FontAwesome5 name='chevron-down'/>
+                </TouchableOpacity>
+              </View>
             </View>
             <View style={styles.boxInput}>
-              <Text>Entradas Operacionais</Text>
-              <Text>-</Text>
-              <Text>0,0%</Text>
-              <FontAwesome5 name='chevron-down'/>
+              <View>
+                <Text style={styles.textInitial}>Entradas Operacionais</Text>
+              </View>
+              <View style={styles.boxMountante}>
+                <Text style={styles.textValor}>-</Text>
+              </View>
+              <View style={styles.boxPorcertn}>
+                <Text style={styles.textPorcen}>0,0%</Text>
+                <TouchableOpacity>
+                  <FontAwesome5 name='chevron-down'/>
+                </TouchableOpacity>
+              </View>
             </View>
             <View style={styles.boxInput}>
-              <Text>Entradas Operacionais</Text>
-              <Text>-</Text>
-              <Text>0,0%</Text>
-              <FontAwesome5 name='chevron-down'/>
+              <View>
+                <Text style={styles.textInitial}>Entradas Operacionais</Text>
+              </View>
+              <View style={styles.boxMountante}>
+                <Text style={styles.textValor}>-</Text>
+              </View>
+              <View style={styles.boxPorcertn}>
+                <Text style={styles.textPorcen}>0,0%</Text>
+                <TouchableOpacity>
+                  <FontAwesome5 name='chevron-down'/>
+                </TouchableOpacity>
+              </View>
             </View>
             <View style={styles.boxInput}>
-              <Text>Entradas Operacionais</Text>
-              <Text>-</Text>
-              <Text>0,0%</Text>
-              <FontAwesome5 name='chevron-down'/>
+              <View>
+                <Text style={styles.textInitial}>Entradas Operacionais</Text>
+              </View>
+              <View style={styles.boxMountante}>
+                <Text style={styles.textValor}>-</Text>
+              </View>
+              <View style={styles.boxPorcertn}>
+                <Text style={styles.textPorcen}>0,0%</Text>
+                <TouchableOpacity>
+                  <FontAwesome5 name='chevron-down'/>
+                </TouchableOpacity>
+              </View>
             </View>
             <View style={styles.boxInput}>
-              <Text>Entradas Operacionais</Text>
-              <Text>-</Text>
-              <Text>0,0%</Text>
-              <FontAwesome5 name='chevron-down'/>
+              <View>
+                <Text style={styles.textInitial}>Entradas Operacionais</Text>
+              </View>
+              <View style={styles.boxMountante}>
+                <Text style={styles.textValor}>-</Text>
+              </View>
+              <View style={styles.boxPorcertn}>
+                <Text style={styles.textPorcen}>0,0%</Text>
+                <TouchableOpacity>
+                  <FontAwesome5 name='chevron-down'/>
+                </TouchableOpacity>
+              </View>
             </View>
-            <View style={styles.boxInput}>
-              <Text>Entradas Operacionais</Text>
-              <Text>-</Text>
-              <Text>0,0%</Text>
-              <FontAwesome5 name='chevron-down'/>
+
+            <View style={styles.boxTotal}>
+              <View>
+                <Text style={styles.textInitial}>TOTAL</Text>
+              </View>
+              <View style={styles.boxPorcertn}>
+                <Text style={styles.textPorcen}>100%</Text>
+              </View>
             </View>
+            
 
           </View>
 
@@ -100,37 +181,68 @@ const styles = StyleSheet.create({
   container:{
     flex:1,
     width:'100%',
-    height:'auto',
-    backgroundColor:'pink',
+    height:'100%',
+    backgroundColor:'#691CC7',
   },
-  
+  scrollContainer:{
+    flex:1,
+    width:'100%',
+  },
   content:{
     flex:1,
     width:'100%',
-    backgroundColor:'#691CC7',
-    
+    height:'100%',
+    alignItems:'center',
+    justifyContent:'space-evenly',
+    paddingTop:15,
+  },
+
+  closeContainer:{
+    width:'100%',
+    alignItems:'flex-end',
+    justifyContent:'flex-end',
   },
   closeBox:{
+    width:125,
     flex:1,
     flexDirection:'row',
     alignItems:'center',
-    justifyContent:'center',
+    justifyContent:'space-evenly',
+    marginRight:25,
+    marginTop:15,
+  },
+  closeText:{
+    color:'#fff',
+    fontSize:11,
+    fontWeight:'bold',
   },
   contentBox:{
-    width:321,
+    width:'90%',
     height:'auto',
+    marginTop:20,
+    alignItems:'flex-start',
+    justifyContent:'center',
+    marginTop:55,
+  },
+  textDetails:{
+    color:'#fff',
+    fontSize:15,
+    marginBottom:10,
   },
   boxWhite:{
     backgroundColor:'#fff',
     borderRadius:10,
     width:'100%',
     height:57,
+    marginVertical:10,
     alignItems:'center',
     justifyContent:'center',
 
   },
   boxText:{
     textAlign:'center',
+    width:214,
+    fontSize:13,
   },
   boxInput:{
     flexDirection:'row',
@@ -140,5 +252,31 @@ const styles = StyleSheet.create({
     alignItems:'center',
     backgroundColor:'#fff',
     borderRadius:10,
+    marginBottom:10,
+  },
+  boxMountante:{
+    borderLeftWidth:1,
+    borderRightWidth:1,
+    justifyContent:'center',
+    alignItems:'center',
+    height:'100%',
+    width:85,
+    borderColor:'#B7B7B7',
+  },
+  boxPorcertn:{
+    flexDirection:'row',
+    width:85,
+    justifyContent:'space-between',
+    alignItems:'center',
+  },
+  boxTotal:{
+    backgroundColor:'#F9A179',
+    flexDirection:'row',
+    width:'100%',
+    height:47,
+    justifyContent:'space-between',
+    alignItems:'center',
+    borderRadius:10,
+    marginBottom:10,
   },
 });
